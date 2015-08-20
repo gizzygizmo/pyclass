f5701f01b9ffe38f8653e53a94ff17f83536472f
Applied Python user <ian.steinmetz@gmail.com>
2015-08-20 13:42:49 -0700

    updated login with subs

--- pytelnet.py
+++ pytelnet.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
+import time
 import telnetlib
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
+
+def cmd(remote_conn, cmd):
+   cmd = cmd.rstrip() # strip off whitespace
+   remote_conn.write(cmd+"\n")
+   time.sleep(1)
+   return remote_conn.read_very_eager()
+
+def tlogin(remote_conn, username, password):
+   result = remote_conn.read_until("rname:", TELNET_TIMEOUT)
+   remote_conn.write(username + '\n')
+   result += remote_conn.read_until("assword:", TELNET_TIMEOUT)
+   remote_conn.write(password + '\n')
+   time.sleep(1)
+   cmd(remote_conn, "term len 0")
 
 def main():
    username = 'pyclass';
@@ -10,6 +25,9 @@ def main():
    ipaddr = '50.76.53.27'
 
    remote_conn = telnetlib.Telnet(ipaddr, TELNET_PORT, TELNET_TIMEOUT)
+   tlogin(remote_conn, username, password)
+   
+   print  cmd(remote_conn, "show version")
 
    remote_conn.close
 
